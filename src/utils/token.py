1ca3daf578d11a9e048ff7b052ee7423bf9ceb3e
a-asror <abduvosiqovasror@icloud.com>
2022-12-27 11:09:00 +0500

    upd: token

--- src/utils/token.py
+++ src/utils/token.py
@@ -1,38 +1,31 @@
-from django.conf import settings
-from django.http import HttpResponse
-from django.utils import timezone
 from datetime import timedelta
+
+from django.conf import settings
+from django.utils import timezone
 from jwt import api_jwt
+
 from src.users.models import JwtModel
 
 
-def token_generator(uid: int, format_time: dict):
-    data_token = {
-        'id': uid,
-        'exp': timezone.localtime(timezone.now()) + timedelta(**format_time),  # minutes, days
-        'iat': timezone.localtime(timezone.now())
-    }
-    token = api_jwt.encode(data_token, settings.SECRET_KEY, algorithm='HS256')
+def token_generator(uid, format_time):
+    data_token = {"id": uid, "exp": timezone.localtime(timezone.now()) + timedelta(**format_time), "iat": timezone.localtime(timezone.now())}  # minutes, days
+    token = api_jwt.encode(data_token, settings.SECRET_KEY, algorithm="HS256")
     return token
 
 
-def _response(access: str, refresh: str, response: HttpResponse):
-    max_age_in_seconds = 2592000  # 30 days
-    # В продакшене надо будет изменить значение domain
-    response.set_cookie(key='_at', value=refresh, domain='127.0.0.1', httponly=True, max_age=max_age_in_seconds)
-    return 200, {'access': access}
+def _response(access: str, refresh: str):
+    return 200, {"access": access, "refresh": refresh}
 
 
 class UpdateTokens:
-
     def __init__(self, request, response):
         self.request = request
         self.response_to = response
 
     @property
     def access_and_refresh_token(self):
-        refresh = token_generator(self.request.user.id, {'days': 30})
-        access = token_generator(self.request.user.id, {'days': 40})
+        refresh = token_generator(self.request.user.id, {"days": 30})
+        access = token_generator(self.request.user.id, {"days": 40})
         return access, refresh
 
     @property
@@ -50,12 +43,12 @@ class UpdateTokens:
         jwt_user = JwtModel.objects.filter(user_id=self.request.user.id).exists()
         if not jwt_user:  # создать новый объект Jwt если его нет
             JwtModel(user_id=self.request.user.id, access=access, refresh=refresh).save()
-            return _response(access, refresh, self.response_to)
+            return _response(access, refresh)
         return self.response  # обновить токены если они есть в базе
 
     @property
     def response(self):
         data, exc = self.check_jwt_model_and_update
         if exc:
-            return _response(data[0], data[1], self.response_to)
-        return 400, {'message': data}
+            return _response(data[0], data[1])
+        return 400, {"message": data}
